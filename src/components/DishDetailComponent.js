698beebc7de50d653752b72b5346e7bb5c660155
Harerimana Egide <egideharerimana085@gmail.com>
2020-12-14 09:56:08 +0200

    initial commit

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+
+export default class DishDetail extends Component {
+  renderDish(dish) {
+    if (dish != null) {
+      return (
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle tag="h6">{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return <div></div>;
+    }
+  }
+
+  renderComments(comments) {
+    return comments == null ? <div></div> :
+      <div>
+        <h4>Comments</h4>
+        {comments.map((comment) => {
+          return (
+            <ul key={comment.id} className="list-unstyled">
+              <li>
+                <p>{comment.comment}</p>
+                <p>
+                  --{comment.author}, {comment.date}
+                </p>
+              </li>
+            </ul>
+          );
+        })}
+      </div>
+  }
+  render() {
+    return (
+      <div className="row mt-5">
+        <div className="col-md-5 col-12 m-1">
+          {this.renderDish(this.props.dish)}
+        </div>
+        <div className="col-md-5 col-12 m-1">
+          {this.renderComments(this.props.dish?.comments)}
+        </div>
+      </div>
+    );
+  }
+}
